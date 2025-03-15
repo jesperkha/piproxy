a225be261c93738406172c35defd533aa0d18040
jesperkha <hammerjesp@gmail.com>
2025-03-15 23:26:47 +0100

    Add status wrapper for response writer

--- server/middleware.go
+++ server/middleware.go
@@ -8,12 +8,24 @@ import (
 
 type Middleware func(http.Handler) http.Handler
 
+type WriterWithStatus struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *WriterWithStatus) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.status = statusCode
+}
+
 func Logger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		originalURL := r.URL.String()
 
-		h.ServeHTTP(w, r)
-		log.Printf("logger: new request %s -> %s took %s", originalURL, r.URL.String(), time.Since(start).String())
+		writer := &WriterWithStatus{w, http.StatusOK}
+		h.ServeHTTP(writer, r)
+
+		log.Printf("logger: [%d] %s -> %s %s", writer.status, originalURL, r.URL.String(), time.Since(start).String())
 	})
 }
